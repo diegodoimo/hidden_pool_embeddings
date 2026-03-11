053c8bd1bfbd593153726081545e515c6a1bf9ef
diegodoimo <doimo.diego@gmail.com>
2026-03-11 17:49:33 +0100

    u

--- train_f2llm_repro.py
+++ train_f2llm_repro.py
@@ -184,7 +184,6 @@ def main():
         gradient_accumulation_steps=1,
         deepspeed_plugin=deepspeed_plugin,
     )
-    accelerator = Accelerator()
 
     args.num_processes = accelerator.num_processes
     accelerator.print(args)
